7d62c88a4673012b6785f7af54748c5523a9d5c7
Zhenrong WANG <zhenrongwang@live.com>
2024-04-05 09:55:23 +0800

    Create filter_uniq_ints.h

---
+++ filter_uniq_ints.h
@@ -0,0 +1,21 @@
+/**
+ * 
+ * This code is distributed under the license: MIT License
+ * Originally written by Zhenrong WANG
+ * mailto: zhenrongwang@live.com 
+ * GitHub: https://github.com/zhenrong-wang
+ * 
+ */
+
+#ifndef FILTER_UNIQ_INTS_H
+#define FILTER_UNIQ_INTS_H
+
+int convert_string_to_positive_num(const char* string);
+int* filter_unique_elems_naive(const int *input_arr, const unsigned int num_elems, unsigned int *num_elems_out, int *err_flag);
+int* filter_unique_elems(const int *input_arr, const unsigned int num_elems, unsigned int *num_elems_out, int *err_flag);
+void print_arr(int *arr, unsigned int num_elems);
+int compare_arr(int *arr_a, int *arr_b, unsigned int num_elems);
+int generate_random_input_arr(int *arr, unsigned int num_elems, unsigned int rand_max);
+int generate_growing_arr(int *arr, unsigned int num_elems);
+
+#endif
