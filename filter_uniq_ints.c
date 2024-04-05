bcee835d915bedda5c425380a70d87930cf8b9ad
zhenrong-wang <zhenrongwang@live.com>
2024-04-05 19:23:12 +0800

    Added a hashtable algorithm

--- filter_uniq_ints.c
+++ filter_uniq_ints.c
@@ -14,6 +14,17 @@
 #include "filter_uniq_ints.h"
 
 /**
+ * The maximum 32-bit signed integer is -2^31 - 2^31, 
+ * aka, -32768 x 65536 to 32768 x 65536
+ * So, any given integer x, x / 65536 < = 32768, and the mod
+ * x % 65536 < 65536, Therefore, the HASH_TABLE_SIZE 
+ * should be 32769 to cover 0 ~ 32768, and the MOD_TABLE_SIZE
+ * should be 65536 to cover 0 ~ 65535
+ */
+#define HASH_TABLE_SIZE 32769
+#define MOD_TABLE_SIZE  65536
+
+/**
  * @brief Convert a string to a posivie number
  *  The string cannot contain characters other than 0~9
  * 
@@ -185,8 +196,95 @@ int* filter_unique_elems(const int *input_arr, const unsigned int num_elems, uns
         return NULL;
     }
     *num_elems_out = j;
-    //printf("%d,,,,,,\n", ii);
-    return output_arr;
+    return final_output_arr;
+}
+
+void free_hash_table(int *hash_table[], unsigned int num_elems) {
+    for(unsigned int i = 0; i < num_elems; i++) {
+        if(hash_table[i] != NULL) {
+            free(hash_table[i]);
+        }
+    }
+}
+
+
+int* filter_unique_elems_ht(const int *input_arr, const unsigned int num_elems, unsigned int *num_elems_out, int *err_flag) {
+    unsigned int i, j = 0;
+    unsigned int tmp_quotient = 0, tmp_mod = 0;
+    int tmp = 0;
+    int *final_output_arr = NULL;
+
+    int *hash_table_base_p[HASH_TABLE_SIZE] = {NULL,};
+    int *hash_table_base_n[HASH_TABLE_SIZE] = {NULL,};
+
+    *err_flag = 0;
+    *num_elems_out = 0;
+
+    if (input_arr == NULL) {
+        *err_flag = -5;
+        return NULL;
+    }
+    if (num_elems < 1){
+        *err_flag = -3;
+        return NULL;
+    }
+    int *output_arr = (int *)calloc(num_elems, sizeof(int));
+    if (output_arr == NULL) {
+        *err_flag = -1;
+        return NULL;
+    }
+    for(i = 0; i < num_elems; i++) {
+        tmp = input_arr[i];
+        tmp_quotient = abs(tmp / MOD_TABLE_SIZE);
+        tmp_mod = abs(tmp % MOD_TABLE_SIZE);
+        if(tmp > 0) {
+            if(hash_table_base_p[tmp_quotient] == NULL) {
+                if((hash_table_base_p[tmp_quotient] = (int *)calloc(MOD_TABLE_SIZE, sizeof(int))) == NULL) {
+                    *err_flag = 1;
+                    goto free_memory;
+                }
+            }
+            if(hash_table_base_p[tmp_quotient] != NULL && (hash_table_base_p[tmp_quotient])[tmp_mod] != 0) {
+                continue;
+            }
+        }
+        else {
+            if(hash_table_base_n[tmp_quotient] == NULL) {
+                if((hash_table_base_n[tmp_quotient] = (int *)calloc(MOD_TABLE_SIZE, sizeof(int))) == NULL) {
+                    *err_flag = 1;
+                    goto free_memory;
+                }
+            }
+            if(hash_table_base_n[tmp_quotient] != NULL && (hash_table_base_n[tmp_quotient])[tmp_mod] != 0) {
+                continue;
+            }
+        }
+        output_arr[j] = tmp;
+        j++;
+        if(tmp > 0) {
+            (hash_table_base_p[tmp_quotient])[tmp_mod] = 1;
+        }
+        else {
+            (hash_table_base_n[tmp_quotient])[tmp_mod] = 1;
+        }
+    }
+
+free_memory:
+    free_hash_table(hash_table_base_p, HASH_TABLE_SIZE);
+    free_hash_table(hash_table_base_n, HASH_TABLE_SIZE);
+    if(*err_flag != 0) {
+        free(output_arr);
+        return NULL;
+    }
+
+    final_output_arr = (int *)realloc(output_arr, j*sizeof(int));
+    if(final_output_arr == NULL) {
+        free(output_arr);
+        *err_flag = 3;
+        return NULL;
+    }
+    *num_elems_out = j;
+    return final_output_arr;
 }
 
 /**
@@ -268,7 +366,6 @@ int generate_random_input_arr(int *arr, unsigned int num_elems, unsigned int ran
             arr[i] = 0 - rand_num;
         }
     }
-    //print_arr(arr, num_elems);
     return 0;
 }
 
@@ -333,7 +430,7 @@ int main(int argc, char** argv) {
     int err_flag = 0;
     unsigned int num_elems_out = 0;
     clock_t start, end;
-    int *out = NULL, *out_naive = NULL;
+    int *out = NULL, *out_naive = NULL, *out_ht = NULL;
     generate_random_input_arr(arr_input,num_elems,rand_max);
 
     printf("RANDOM ARRAY INPUT:\n");
@@ -347,9 +444,15 @@ int main(int argc, char** argv) {
     end = clock();
     printf("NAIVE_ALGO:\t%lf\t%d\n", (double)(end - start)/CLOCKS_PER_SEC, num_elems_out);
 
-    printf("COMPARE:\t%d\n\n", compare_arr(out, out_naive, num_elems_out));
+    start = clock();
+    out_ht = filter_unique_elems_ht(arr_input, num_elems, &num_elems_out, &err_flag);
+    end = clock();
+    printf("HASH_ALGO:\t%lf\t%d\n", (double)(end - start)/CLOCKS_PER_SEC, num_elems_out);
+
+    printf("COMPARE:\t%d\t%d\n\n", compare_arr(out, out_naive, num_elems_out), compare_arr(out, out_ht, num_elems_out));
     free(out);
     free(out_naive);
+    free(out_ht);
 
     generate_growing_arr(arr_input,num_elems);
     printf("GROWING ARRAY INPUT:\n");
@@ -363,9 +466,14 @@ int main(int argc, char** argv) {
     end = clock();
     printf("NAIVE_ALGO:\t%lf\t%d\n", (double)(end - start)/CLOCKS_PER_SEC, num_elems_out);
 
-    printf("COMPARE:\t%d\n", compare_arr(out, out_naive, num_elems_out));
+    start = clock();
+    out_ht = filter_unique_elems_ht(arr_input, num_elems, &num_elems_out, &err_flag);
+    end = clock();
+    printf("HASH_ALGO:\t%lf\t%d\n", (double)(end - start)/CLOCKS_PER_SEC, num_elems_out);
+
+    printf("COMPARE:\t%d\t%d\n\n", compare_arr(out, out_naive, num_elems_out), compare_arr(out, out_ht, num_elems_out));
     free(out);
-    free(out_naive);
+    free(out_ht);
     
     free(arr_input);
     return 0;
