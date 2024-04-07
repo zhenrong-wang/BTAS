32aa5b15365053815a7e6bf0b67c2e4a52f01127
zhenrong-wang <zhenrongwang@live.com>
2024-04-07 19:50:10 +0800

    Added bit-based hashmap algo

--- filter_uniq_ints.c
+++ filter_uniq_ints.c
@@ -881,11 +881,14 @@ free_memory:
  *  
  */
 int main(int argc, char** argv) {
+    int with_brute = 0;
     if(argc < 3) {
-        printf("ERROR: not enough args. USAGE: ./command argv[1] argv[2].\n");
+        printf("ERROR: not enough args. USAGE: ./command argv[1] argv[2] argv[3](Optional: brute).\n");
         return 1;
     }
-
+    if (argc > 3 && strcmp(argv[3], "brute") == 0) {
+        with_brute = 1;
+    }
     int num_elems = convert_string_to_positive_num(argv[1]), rand_max = convert_string_to_positive_num(argv[2]);
     printf("INPUT_ELEMS:\t%d\nRANDOM_MAX:\t%d\n\n",num_elems, rand_max);
     if(num_elems < 0 || rand_max < 0) {
@@ -926,25 +929,28 @@ int main(int argc, char** argv) {
     end = clock();
     printf("HASH_ALGO_DYN:\t%lf\t%d\n", (double)(end - start)/CLOCKS_PER_SEC, num_elems_out);
 
-    start = clock();
-    out_naive_improved = filter_unique_elems_naive_improved(arr_input, num_elems, &num_elems_out, &err_flag);
-    end = clock();
-    printf("NAIVE_ALGO_NEW:\t%lf\t%d\n", (double)(end - start)/CLOCKS_PER_SEC, num_elems_out);
+    if(with_brute == 1) {
+        start = clock();
+        out_naive_improved = filter_unique_elems_naive_improved(arr_input, num_elems, &num_elems_out, &err_flag);
+        end = clock();
+        printf("NAIVE_ALGO_NEW:\t%lf\t%d\n", (double)(end - start)/CLOCKS_PER_SEC, num_elems_out);
     
-    start = clock();
-    out_naive = filter_unique_elems_naive(arr_input, num_elems, &num_elems_out, &err_flag);
-    end = clock();
-    printf("NAIVE_ALGO:\t%lf\t%d\n", (double)(end - start)/CLOCKS_PER_SEC, num_elems_out);
-    
-    printf("COMPARISON:\t%d\t%d\t%d\t%d\t%d\n\n", compare_arr(out_ht_bit, out_ht, num_elems_out), compare_arr(out_ht_bit, out_ht_new, num_elems_out), compare_arr(out_ht_bit, out_ht_dyn, num_elems_out), compare_arr(out_ht_bit, out_naive_improved, num_elems_out), compare_arr(out_ht_bit, out_naive, num_elems_out));
-    free(out_naive_improved);
-    free(out_naive);
+        start = clock();
+        out_naive = filter_unique_elems_naive(arr_input, num_elems, &num_elems_out, &err_flag);
+        end = clock();
+        printf("NAIVE_ALGO:\t%lf\t%d\n", (double)(end - start)/CLOCKS_PER_SEC, num_elems_out);
+    }
+
     free(out_ht);
     free(out_ht_new);
     free(out_ht_dyn);
     free(out_ht_bit);
-    free(arr_input);
-
+    if(with_brute == 1) {
+        free(out_naive_improved);
+        free(out_naive);
+    }
+    //free(arr_input);
+    memset(arr_input, 0, num_elems);
     generate_growing_arr(arr_input, num_elems);
     printf("GROWING ARRAY INPUT:\n");
     printf("ALGO_TYPE\tTIME_IN_SEC\tUNIQUE_INTEGERS\n");
@@ -974,15 +980,15 @@ int main(int argc, char** argv) {
     end = clock();
     printf("NAIVE_ALGO_NEW:\t%lf\t%d\n", (double)(end - start)/CLOCKS_PER_SEC, num_elems_out);
     
-    /* The NAIVE algo is unacceptable for this case. Skip it. */
-    /*start = clock();
-    out_naive = filter_unique_elems_naive(arr_input, num_elems, &num_elems_out, &err_flag);
-    end = clock();
-    printf("NAIVE_ALGO:\t%lf\t%d\n", (double)(end - start)/CLOCKS_PER_SEC, num_elems_out);*/
+    if(with_brute == 1) {
+        start = clock();
+        out_naive = filter_unique_elems_naive(arr_input, num_elems, &num_elems_out, &err_flag);
+        end = clock();
+        printf("NAIVE_ALGO:\t%lf\t%d\n", (double)(end - start)/CLOCKS_PER_SEC, num_elems_out);
+        free(out_naive);
+    }
     
-    printf("COMPARISON:\t%d\t%d\t%d\t%d\n\n", compare_arr(out_ht_bit, out_ht, num_elems_out), compare_arr(out_ht_bit, out_ht_new, num_elems_out), compare_arr(out_ht_bit, out_ht_dyn, num_elems_out), compare_arr(out_ht_bit, out_naive_improved, num_elems_out));
     free(out_naive_improved);
-    //free(out_naive);
     free(out_ht);
     free(out_ht_new);
     free(out_ht_dyn);
