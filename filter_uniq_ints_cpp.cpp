a47c35ec0010c6c13154d3486e18cdedcc7d4655
Kevin Channon <kevin.channon@paconsulting.com>
2024-04-07 08:20:11 +0100

    Add identity hashing function for ints

--- filter_uniq_ints_cpp.cpp
+++ filter_uniq_ints_cpp.cpp
@@ -1,8 +1,15 @@
 #include "filter_uniq_ints_cpp.hpp"
 
 #include <unordered_set>
+#include <algorithm>
+
+namespace {
+struct IdentityHash {
+  [[nodiscard]] std::size_t operator()(int i) const noexcept { return i; }
+};
+}
 
 std::vector<int> filter_uniq_cpp(std::span<const int> in) {
-  const auto filtered = std::unordered_set<int>{in.begin(), in.end()};
+  const auto filtered = std::unordered_set<int, IdentityHash>{in.begin(), in.end()};
   return {filtered.begin(), filtered.end()};
 }
