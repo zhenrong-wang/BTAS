9202512f7047ff70e407c90c61ba5f6935bc0bc4
zhenrong-wang <zhenrongwang@live.com>
2024-04-17 12:16:46 +0800

    Added data loader empty file

---
+++ data_loader.h
@@ -0,0 +1,8 @@
+/**
+ * 
+ * This code is distributed under the license: MIT License
+ * Originally written by Zhenrong WANG
+ * mailto: zhenrongwang@live.com 
+ * GitHub: https://github.com/zhenrong-wang
+ * 
+ */
